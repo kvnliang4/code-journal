7f6c8b8aa359354afe90e534fb7292d770d9f869
Kevin Liang <kevinliang415@yahoo.com>
2022-05-31 06:50:29 +0000

    completed 6, 7, and 12. Working on steps inbetween

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ photoUrl.addEventListener('input', photoPreview);
 function photoPreview(event) {
   picture.src = event.target.value;
 }
+
+var formValues = document.querySelector('#entry-form');
+
+formValues.addEventListener('submit', save);
+
+function save(event) {
+  event.preventDefault();
+  /*  var entry = {
+    title: document.querySelector('#title').value,
+    notes: document.querySelector('#notes').value,
+    photoUrl: document.querySelector('.photoUrl').value
+  };
+  console.log(entry); */
+  formValues.reset();
+}
