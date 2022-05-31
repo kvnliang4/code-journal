f37fdc30ac607b417f751b926dc5e16a6b3a8d51
Kevin Liang <kevinliang415@yahoo.com>
2022-05-31 19:42:04 +0000

    fixed screen conversion issues, completed adding nextEntryId and resetting src preview

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ photoUrl.addEventListener('input', photoPreview);
 
 function photoPreview(event) {
   picture.src = event.target.value;
+  if (event.target.value.length === 0) {
+    picture.src = 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640';
+  }
 }
 
 var formValues = document.querySelector('#entry-form');
@@ -16,8 +19,11 @@ function save(event) {
   /*  var entry = {
     title: document.querySelector('#title').value,
     notes: document.querySelector('#notes').value,
-    photoUrl: document.querySelector('.photoUrl').value
+    photoUrl: document.querySelector('.photoUrl').value,
+    nextEntryId: 0
   };
+  entry.nextEntryId++;
   console.log(entry); */
   formValues.reset();
+  picture.src = 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640';
 }
