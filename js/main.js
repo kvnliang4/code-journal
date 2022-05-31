cdec49a5d2725a2a09085b320ad1de95a7c8e526
Kevin Liang <kevinliang415@yahoo.com>
2022-05-31 00:06:58 +0000

    forgot to commit, tasks 1-5 completed

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var picture = document.querySelector('.picture');
+var photoUrl = document.querySelector('.photoUrl');
+
+photoUrl.addEventListener('input', photoPreview);
+
+function photoPreview(event) {
+  picture.src = event.target.value;
+}
