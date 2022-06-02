e1f78e9b28ebfcad63cc1e9290dfd9e856306fa4
Kevin Liang <kevinliang415@yahoo.com>
2022-06-02 01:20:04 +0000

    dom tree for new entries appended successfully

--- js/main.js
+++ js/main.js
@@ -27,3 +27,40 @@ function save(event) {
   data.nextEntryId++;
   picture.src = 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640';
 }
+
+function renderEntries(entry) {
+  var li = document.createElement('li');
+  var rowDiv = document.createElement('div');
+  rowDiv.className = 'row';
+  li.appendChild(rowDiv);
+  var imgColumnDiv = document.createElement('div');
+  imgColumnDiv.className = 'column-half';
+  rowDiv.appendChild(imgColumnDiv);
+  var img = document.createElement('img');
+  imgColumnDiv.appendChild(img);
+  img.src = entry.photoUrl;
+  var textColumnDiv = document.createElement('div');
+  textColumnDiv.className = 'column-half';
+  rowDiv.appendChild(textColumnDiv);
+  var title = document.createElement('span');
+  var text = document.createElement('p');
+  textColumnDiv.appendChild(title);
+  textColumnDiv.appendChild(text);
+  var entryTitle = document.createTextNode(entry.title);
+  title.appendChild(entryTitle);
+  var entryNotes = document.createTextNode(entry.notes);
+  text.appendChild(entryNotes);
+  return li;
+}
+
+var ul = document.querySelector('ul');
+
+window.addEventListener('DOMContentLoaded', appendDomContent);
+
+function appendDomContent(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    if (data.entries[i] !== null) {
+      ul.appendChild(renderEntries((data.entries[i])));
+    }
+  }
+}
